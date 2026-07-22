cc1d781f58bb8526a6c48a3a97fa5628b6247377
Zyn <bobbytatum999@gmail.com>
2026-07-22 10:02:26 -0500

    Expose honest core capability classification

--- CoreBridge/RPCS3CoreBridge.h
+++ CoreBridge/RPCS3CoreBridge.h
@@ -12,14 +12,24 @@ typedef enum RPCS3IOSCoreState {
     RPCS3IOSCoreStateFailed = 4
 } RPCS3IOSCoreState;
 
+typedef enum RPCS3IOSCoreCapabilityLevel {
+    RPCS3IOSCoreCapabilityProbeOnly = 0,
+    RPCS3IOSCoreCapabilityPartialUpstream = 1,
+    RPCS3IOSCoreCapabilityExecutionCapable = 2
+} RPCS3IOSCoreCapabilityLevel;
+
 typedef struct RPCS3IOSCoreDiagnostics {
     RPCS3IOSCoreState state;
+    RPCS3IOSCoreCapabilityLevel capability_level;
     int platform_initialized;
     int upstream_crypto_available;
+    int upstream_source_count;
     int ppu_interpreter_available;
     int spu_interpreter_available;
     int jit_available;
     int renderer_available;
+    const char *upstream_revision;
+    const char *build_classification;
     const char *data_path;
     const char *last_boot_sha256;
     const char *message;
