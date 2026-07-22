cf6ac1fec4713bfc6374689a56c59339a807d166
Zyn <bobbytatum999@gmail.com>
2026-07-22 14:19:43 -0500

    Track live Metal RSX program pipelines

--- Renderers/Metal/RPCS3MetalGSRender.h
+++ Renderers/Metal/RPCS3MetalGSRender.h
@@ -2,8 +2,11 @@
 
 #include "Emu/RSX/GSRender.h"
 #include "RPCS3MetalPipelineState.h"
+#include "RPCS3MetalProgramCompiler.h"
 #include "RPCS3MetalRenderer.h"
 #include "RPCS3MetalRSXFormats.h"
+
+#include <string>
 
 namespace rpcs3::ios::render
 {
@@ -21,14 +24,19 @@ private:
     void end() override;
     bool initialize_backend();
     void capture_rsx_draw_state();
+    bool prepare_live_program_pipeline();
 
     metal_renderer m_backend;
     bool m_backend_initialized = false;
     u64 m_presented_frames = 0;
     u64 m_translated_draws = 0;
     u64 m_topology_rewrite_draws = 0;
+    u64 m_program_ready_draws = 0;
+    u64 m_program_compile_failures = 0;
     metal_rsx::primitive_mapping m_primitive_mapping{};
     metal_rsx::depth_stencil_state m_depth_stencil_state{};
     metal_rsx::color_blend_state m_color_blend_state{};
+    metal_rsx::compiled_render_pipeline m_active_pipeline{};
+    std::string m_last_program_error;
 };
 } // namespace rpcs3::ios::render
