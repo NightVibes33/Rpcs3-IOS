e75ac9f283818bc0b3c15d2b596b7ee2706fee40
Zyn <bobbytatum999@gmail.com>
2026-07-21 23:34:40 -0500

    Add RPCS3 core bridge API scaffold

---
+++ CoreBridge/RPCS3CoreBridge.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum RPCS3IOSCoreState {
+    RPCS3IOSCoreStateUnavailable = 0,
+    RPCS3IOSCoreStateReady = 1,
+    RPCS3IOSCoreStateRunning = 2,
+    RPCS3IOSCoreStateStopped = 3,
+    RPCS3IOSCoreStateFailed = 4
+} RPCS3IOSCoreState;
+
+typedef struct RPCS3IOSCoreDiagnostics {
+    RPCS3IOSCoreState state;
+    int ppu_interpreter_available;
+    int spu_interpreter_available;
+    int jit_available;
+    int renderer_available;
+    const char *message;
+} RPCS3IOSCoreDiagnostics;
+
+RPCS3IOSCoreDiagnostics rpcs3_ios_core_diagnostics(void);
+int rpcs3_ios_core_initialize(const char *data_path);
+int rpcs3_ios_core_boot_elf(const char *elf_path);
+void rpcs3_ios_core_stop(void);
+
+#ifdef __cplusplus
+}
+#endif
