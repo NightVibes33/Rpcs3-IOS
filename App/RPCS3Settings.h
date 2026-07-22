63a576b4ab84115acb287cca14d85a8e9beec236
Zyn <bobbytatum999@gmail.com>
2026-07-22 01:37:28 -0500

    Add adaptive RPCS3 settings controllers

---
+++ App/RPCS3Settings.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RPCS3SettingsController : UITableViewController
+@end
+
+@interface RPCS3PadsController : UITableViewController
+@end
+
+NS_ASSUME_NONNULL_END
