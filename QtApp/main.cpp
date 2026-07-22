4cdcd2952814e4be6c8d912bd00ca0e52c43ad62
Zyn <bobbytatum999@gmail.com>
2026-07-22 10:31:45 -0500

    Anchor Qt build identity in the executable

--- QtApp/main.cpp
+++ QtApp/main.cpp
@@ -6,6 +6,10 @@
 #include <QMessageBox>
 #include <QTimer>
 
+#ifndef RPCS3_QT_BUILD_MARKER
+#define RPCS3_QT_BUILD_MARKER "RPCS3 Qt iOS upstream main_window.ui"
+#endif
+
 int main(int argc, char* argv[])
 {
     QApplication application(argc, argv);
@@ -13,6 +17,7 @@ int main(int argc, char* argv[])
     QCoreApplication::setApplicationVersion(QStringLiteral("0.1.0"));
     QCoreApplication::setOrganizationName(QStringLiteral("NightVibes33"));
     QCoreApplication::setOrganizationDomain(QStringLiteral("com.nightvibes33"));
+    application.setProperty("RPCS3QtBuildMarker", QString::fromLatin1(RPCS3_QT_BUILD_MARKER));
 
     const int initialized = rpcs3_ios_core_initialize(nullptr);
 
