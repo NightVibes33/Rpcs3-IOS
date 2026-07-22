a1b30243e356d00d339ee7167c2c905768b06a08
Zyn <bobbytatum999@gmail.com>
2026-07-22 14:40:10 -0500

    Track staged RPCS3 geometry in Metal renderer

--- Renderers/Metal/RPCS3MetalGSRender.h
+++ Renderers/Metal/RPCS3MetalGSRender.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Emu/RSX/GSRender.h"
+#include "RPCS3MetalGeometryStaging.h"
 #include "RPCS3MetalPipelineState.h"
 #include "RPCS3MetalProgramCompiler.h"
 #include "RPCS3MetalRenderer.h"
@@ -25,6 +26,7 @@ private:
     bool initialize_backend();
     void capture_rsx_draw_state();
     bool prepare_live_program_pipeline();
+    bool stage_live_geometry();
 
     metal_renderer m_backend;
     bool m_backend_initialized = false;
@@ -33,10 +35,15 @@ private:
     u64 m_topology_rewrite_draws = 0;
     u64 m_program_ready_draws = 0;
     u64 m_program_compile_failures = 0;
+    u64 m_geometry_ready_draws = 0;
+    u64 m_geometry_stage_failures = 0;
+    rsx::vertex_input_layout m_vertex_layout{};
+    metal_rsx::staged_geometry m_staged_geometry{};
     metal_rsx::primitive_mapping m_primitive_mapping{};
     metal_rsx::depth_stencil_state m_depth_stencil_state{};
     metal_rsx::color_blend_state m_color_blend_state{};
     metal_rsx::compiled_render_pipeline m_active_pipeline{};
     std::string m_last_program_error;
+    std::string m_last_geometry_error;
 };
 } // namespace rpcs3::ios::render
