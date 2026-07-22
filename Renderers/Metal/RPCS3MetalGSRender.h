e4cd27ca2c4fe171bab4ea508315e15a89f784bc
Zyn <bobbytatum999@gmail.com>
2026-07-22 14:59:38 -0500

    Track converted live RSX geometry packets

--- Renderers/Metal/RPCS3MetalGSRender.h
+++ Renderers/Metal/RPCS3MetalGSRender.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Emu/RSX/GSRender.h"
+#include "RPCS3MetalGeometryPacket.h"
 #include "RPCS3MetalPipelineState.h"
 #include "RPCS3MetalProgramCompiler.h"
 #include "RPCS3MetalRenderer.h"
@@ -25,6 +26,7 @@ private:
     bool initialize_backend();
     void capture_rsx_draw_state();
     bool prepare_live_program_pipeline();
+    bool prepare_live_geometry_packet();
 
     metal_renderer m_backend;
     bool m_backend_initialized = false;
@@ -35,6 +37,8 @@ private:
     u64 m_program_compile_failures = 0;
     u64 m_program_cache_hits = 0;
     u64 m_program_cache_misses = 0;
+    u64 m_geometry_ready_draws = 0;
+    u64 m_geometry_failures = 0;
     usz m_cached_vertex_program_hash = umax;
     usz m_cached_fragment_program_hash = umax;
     bool m_cached_program_pair_valid = false;
@@ -44,6 +48,9 @@ private:
     metal_rsx::depth_stencil_state m_depth_stencil_state{};
     metal_rsx::color_blend_state m_color_blend_state{};
     metal_rsx::compiled_render_pipeline m_active_pipeline{};
+    rsx::vertex_input_layout m_vertex_layout{};
+    metal_rsx::geometry_packet m_geometry_packet{};
     std::string m_last_program_error;
+    std::string m_last_geometry_error;
 };
 } // namespace rpcs3::ios::render
