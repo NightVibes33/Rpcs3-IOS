92f03b48f4cb2229a2d877ece2880c955c6caf74
Zyn <bobbytatum999@gmail.com>
2026-07-22 00:34:49 -0500

    Expose SELF metadata and segment diagnostics

--- Port/RPCS3SELFProbe.h
+++ Port/RPCS3SELFProbe.h
@@ -10,8 +10,15 @@ struct self_probe_result
     bool recognized = false;
     bool structurally_valid = false;
     bool contains_plain_elf = false;
+    bool metadata_layout_valid = false;
+    bool requires_decryption = false;
     std::uint64_t elf_offset = 0;
     std::uint64_t file_size = 0;
+    std::uint64_t metadata_offset = 0;
+    std::uint64_t segment_table_offset = 0;
+    std::uint32_t segment_count = 0;
+    std::uint32_t encrypted_segment_count = 0;
+    std::uint32_t compressed_segment_count = 0;
     std::string description;
 };
 
